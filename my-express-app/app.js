3aa65065b848254919b949d2261307485b8c8dfa
muhammadtegarramadhani2327240067-debug <muhammadtegarramadhani_2327240067@mhs.mdp.ac.id>
2025-10-10 21:14:31 +0700

    test

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -3,24 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//panggil file koneksi database
+require("./app_toko_online/models/db");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+//Perbaikan ke 2
+var indexRouter = require('./app_toko_online/routes/index');
+var usersRouter = require('./app_toko_online/routes/users');
+var productRouter = require("./app_toko_online/routes/product"); //letakkan di atas agar rapi
+var engine = require('ejs-blocks'); //menggunakan ejs block
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'app_toko_online', 'views')); //perbaikan 1
+app.engine('ejs', engine);  //daftarkan engine ejs block
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//serving bootstrap
+app.use('/bootstrap', express.static(path.join(__dirname,'node_modules/bootstrap/dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/produk", productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
